e08cb7544d0e9d3e2cf13c89505eda6a7a002700
Aleksander Hansen <aleksanderhan@gmail.com>
2016-11-08 18:17:43 +0100

    first models and a test view

--- algobin/core/models.py
+++ algobin/core/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Algorithm(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=600)
+	pub_date = models.DateTimeField('date published')
+
+
+class LineOfCode(models.Model):
+	algorithm = models.ForeignKey(Algorithm, on_delete=models.CASCADE)
+	lineText = models.CharField(max_length=150)
